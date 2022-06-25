d681574d66491af2d9af7dc88717436a86ae9fab
Leralix <78090544+Leralix@users.noreply.github.com>
2022-06-25 18:31:42 +0200

    Mouvement v1

--- Assets/Scenes/Mouvement.cs
+++ Assets/Scenes/Mouvement.cs
@@ -4,15 +4,106 @@ using UnityEngine;
 
 public class Mouvement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public int movementSpeed;
+    public int movementRatio;
+    private float mouvementMultiplier;
+
+    public int jumpPower;
+    private bool isGrounded, isOnWallRight, isOnWallLeft;
+    public Rigidbody2D Body;
+
+    public Transform groundCheck1, groundCheck2, wallCheckUpRight, wallCheckDownRight, wallCheckUpLeft, wallCheckDownLeft;
+    public LayerMask Platform;
+
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+
         
+    }
+
+    void MovePlayer()
+    {
+        isGrounded = Physics2D.OverlapArea(groundCheck1.position, groundCheck2.position, Platform);
+        
+        isOnWallRight = Physics2D.OverlapCircle(wallCheckUpRight.position, 0.2f, Platform) && Physics2D.OverlapCircle(wallCheckDownRight.position, 0.2f, Platform);
+        isOnWallLeft = Physics2D.OverlapCircle(wallCheckUpLeft.position, 0.2f, Platform) && Physics2D.OverlapCircle(wallCheckDownLeft.position, 0.2f, Platform);
+
+        if (Input.GetKey("d"))
+        {
+
+            if(mouvementMultiplier < 0)
+            {
+                mouvementMultiplier += Time.deltaTime * movementRatio;
+            }
+            else
+            {
+                mouvementMultiplier += Time.deltaTime * (movementRatio * 2);
+            }
+
+        }
+        if (Input.GetKey("q"))
+        {
+            if (mouvementMultiplier > 0)
+            {
+                mouvementMultiplier -= Time.deltaTime * movementRatio;
+            }
+            else
+            {
+                mouvementMultiplier -= Time.deltaTime * (movementRatio * 2);
+            }
+            
+        }
+        if (!Input.GetKey("q") && !Input.GetKey("q"))
+        {
+            if (mouvementMultiplier > 0)
+            {
+                mouvementMultiplier -= Time.deltaTime;
+            }
+            if (mouvementMultiplier < 0)
+            {
+                mouvementMultiplier += Time.deltaTime;
+            }
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            if(isGrounded)
+            {
+                Body.AddForce(Vector2.up * jumpPower);
+            }
+            else if(isOnWallRight)
+            {
+                Body.AddForce(Vector2.up * jumpPower);
+                mouvementMultiplier = -1f;
+            }
+            else if(isOnWallLeft)
+            {
+                Body.AddForce(Vector2.up * jumpPower);
+                mouvementMultiplier = 1f;
+            }
+        }
+
+        //Body.AddForce(Vector2.right * movementSpeed * (mouvementMultiplier + 0.5f) * MovementDirecton);
+
+        mouvementMultiplier = Mathf.Clamp(mouvementMultiplier, -1f, 1f);
+        /*if (mouvementMultiplier < -1f)
+        {
+            mouvementMultiplier = -1;
+        }
+        else if (mouvementMultiplier > 1f)
+        {
+            mouvementMultiplier = 1;
+        }*/
+
+
+        Body.velocity = new Vector2(movementSpeed * (mouvementMultiplier), Body.velocity.y);
+        
+    }
+    void verticalMovement(int direction, int ratio)
+    {
+
     }
 }
