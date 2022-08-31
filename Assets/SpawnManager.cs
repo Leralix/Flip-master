ea1a8e70cfa490896e83e5da6529fcec58f3d5cd
Leralix <78090544+Leralix@users.noreply.github.com>
2022-08-31 16:36:14 +0200

    Point de réaparition partie 1/2

---
+++ Assets/SpawnManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public static SpawnManager instance;
+
+    SpawnPoint[] spawnpoints;
+
+    private void Awake()
+    {
+        instance = this;
+        spawnpoints = GetComponentsInChildren<SpawnPoint>();
+    }
+
+    public SpawnPoint getSpawnPoint()
+    {
+        return spawnpoints[Random.Range(0, spawnpoints.Length)]; 
+    }
+
+}
